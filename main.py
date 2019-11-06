2832fdb100c356ca0dcd91f2bdfd64c9d04ccba7
patrickluong <39811672+patrickluong@users.noreply.github.com>
2019-11-07 01:31:56 +0900

    Moved main.py to repo

---
+++ main.py
@@ -0,0 +1,55 @@
+import sys
+import datetime
+import os
+import webbrowser
+import praw
+
+# input format:
+# python main.py {required bool: auto-open browser} {optional int: posts limit}
+
+def main():
+    mapping = {
+        'True' : 1,
+        'true' : 1,
+        't': 1,
+        '1' : 1,
+        'False' : 0,
+        'false' : 0,
+        'f' : 0,
+        '0' : 0
+    }
+
+    should_open_browser = None
+    posts_limit = 10
+    reddit = praw.Reddit('kindle-freebies-collector')
+
+    # sys.argv does not include 'python'
+    should_open_browser = bool(mapping[sys.argv[1]])
+    
+    if len(sys.argv) > 2:
+        posts_limit = int(sys.argv[2])
+
+    chrome_path = None
+    browser = None
+    first = None
+
+    if should_open_browser:  
+        chrome_path = r"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s --incognito"
+        browser = webbrowser.get(chrome_path)
+        first = True
+
+    for submission in reddit.subreddit('KindleFreebies').new(limit=posts_limit):
+        print('\n' + '-' * 52 + ' POST ' + '-' * 52 + '\n')        
+        print(submission.title + '\n')
+        print(datetime.datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S') + ' UTC\n')
+        print(submission.url + '\n')
+        print('-' * 110)
+
+        if browser != None:
+            browser.open_new_tab(submission.url)
+
+    print()
+
+
+if __name__ == '__main__':
+    main()
